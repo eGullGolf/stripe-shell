7bd7e97849e2c6e9a987910ca2976c86ea748974
Eric Bréchemier <eric@egull.co>
2016-11-23 13:01:02 +0100

    Add script to send generic requests

---
+++ request.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Send a generic request to Stripe API using cUrl
+#
+# Parameters:
+#   $1 - string, name of the HTTP method: 'GET', 'POST', 'DELETE',...
+#   $2 - string, name of the API end-point
+#   ... - API parameters in the form param='value',
+#         where the value is URL-encoded and quoted if needed for safety
+#
+cd "$(dirname "$0")"
+
+. ./config.sh
+
+httpMethod="$1"
+stripeApiName="$2"
+if test -z "$httpMethod" -o -z "$stripeApiName"
+then
+  echo "Usage: $0 httpMethod stripeApiName [param='value']..." 1>&2
+  exit 1
+fi
+
+stripeApiBaseUrl='https://api.stripe.com/v1'
+
+{
+  echo "$stripeApiBaseUrl/$stripeApiName"
+  echo "-u '$stripeSecretKey:'"
+
+  shift 2
+  for stripeKeyValue in $*
+  do
+    echo "-d $stripeKeyValue"
+  done
+} | xargs curl --request "$httpMethod"

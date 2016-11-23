bcabc824ef66d07d3aa87a4145e8afc236a21a8d
Eric Bréchemier <eric@egull.co>
2016-11-23 12:12:26 +0100

    Load custom configuration after definition of default values
    I have decided to do both in a single file, which will be imported
    in a single step from each script which correspond to a separate
    API method.

--- config.sh
+++ config.sh
@@ -1,2 +1,10 @@
+# Default Parameters
+
 # Secret API Key
 stripeSecretKey='sk_test_kInGXJ4LFZjhbe74zoGSdoJb:' # Test Key from Stripe Docs
+
+# Global Configuration
+test -f /etc/stripe-shell/config.sh && . /etc/stripe-shell/config.sh
+
+# Local Configuration
+test -f ./config.my.sh && . ./config.my.sh

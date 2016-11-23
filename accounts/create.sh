5bc1a1fedb23689982f007c0b199bd8a09af0d4c
Eric Bréchemier <eric@egull.co>
2016-11-23 14:54:57 +0100

    Add API method to create merchant accounts for Stripe Connect

---
+++ accounts/create.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Create an account for a merchant member of your platform
+#
+# Parameters:
+#   email - string, the email address of the merchant
+#   ... - see [1] for the list of optional parameters
+#
+# Output:
+#   newly created account object, in JSON format
+#
+# References:
+# [1] Stripe API > Connect > Create an account
+# https://stripe.com/docs/api#create_account
+
+# [2] Stripe Docs > Deferred Account Activation > Creating a standalone account
+# https://stripe.com/docs/connect/deferred-accounts
+#
+cd "$(dirname "$0")"
+
+../request.sh POST accounts $*

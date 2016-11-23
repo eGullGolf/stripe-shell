42c30d6e581207c545c8860c43ead9f93441ee86
Eric Bréchemier <eric@egull.co>
2016-11-23 12:41:07 +0100

    Remove ':' from value of Secret Key in config
    This is a separator for the empty password field;
    it is not part of the secret key value itself.

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 # Default Parameters
 
 # Secret API Key
-stripeSecretKey='sk_test_kInGXJ4LFZjhbe74zoGSdoJb:' # Test Key from Stripe Docs
+stripeSecretKey='sk_test_kInGXJ4LFZjhbe74zoGSdoJb' # Test Key from Stripe Docs
 
 # Global Configuration
 test -f /etc/stripe-shell/config.sh && . /etc/stripe-shell/config.sh
